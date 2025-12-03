63705ccef0c6b2aa2701867c320dd50438800b34
Shkodni <132187254+Shkodn@users.noreply.github.com>
2025-12-03 19:27:16 +0300

    Add files via upload

--- java.js
+++ java.js
@@ -2,3 +2,20 @@
 Telegram.WebApp.ready();
 Telegram.WebApp.expand();
 
+$('.Pribl').click(function() {
+        currentOperation = 'plus';
+        $('#modalTitle').text('Введите сумму прибыли');
+        $('#amountInput').val('');
+        $('#commentInput').val('');
+        $('#amountModal').addClass('active');
+        $('#amountInput').focus();
+    });
+
+$('.Minus').click(function() {
+    currentOperation = 'minus';
+    $('#modalTitle').text('Введите сумму расхода');
+    $('#amountInput').val('');
+    $('#commentInput').val('');
+    $('#amountModal').addClass('active');
+    $('#amountInput').focus();
+});
